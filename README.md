84df06b0cb75e13e14629873ecd585899fb44fe0
David Lucero <dvlucero85@gmail.com>
2021-07-22 20:54:50 +0800

    adjustments to edit params before using strong + readme + setup tests

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # sample_rails_api
+
 Sample pure API made on rails
+
+## Using the API
+
+### Postman
+I have tested this app using Postman. Included in this repository is a postman collection of the two payloads provided in the spec.
--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -6,12 +6,15 @@ class ReservationsController < ApplicationController
   end
 
   def create
-    guest = Guest.create(guest_attributes)
-    @reservation = Reservation.new(reservation_params)
+    guest = Guest.create(guest_attributes(params))
+    @reservation = Reservation.new(reservation_attributes(params))
     @reservation.guest = guest
 
     if @reservation.save
-      render json: @reservation
+      render json: {
+        reservation: @reservation,
+        guest: guest
+      }
     else 
       render json: { status: @reservation.error }
     end
@@ -41,52 +44,53 @@ class ReservationsController < ApplicationController
 
   private
 
-  def reservation_params 
-    att = params.require(:reservation).permit(
-      :start_date, :end_date, :nights, :guests, :adults, :children, :infants, :status, :currency, :payout_price, :security_price, :total_price,
-      :expected_payout_amount,
-      :listing_security_price_accurate, :host_currency, :number_of_guests, :status_type, :total_paid_amount_accurate,
-      guest_details: %i[localized_description number_of_adults number_of_children number_of_infants],
+  def reservation_attributes(params)
+    reservation_params(params).permit(
+      :start_date, :end_date, :nights, :number_of_guests, :adults, :children,
+      :infants, :status, :currency,:payout_price, :security_price, :total_price, :description
     )
-
-    {
-      'start_date' => att['start_date'],
-      'end_date' => att['end_date'],
-      'nights' => att['nights'],
-      'number_of_guests' => att['guests'] || att['number_of_guests'],
-      'status' => att['status'] || att['status_type'],
-      'currency' => att['currency'] || att['host_currency'],
-      'security_price' => att['security_price'] || att['listing_security_price_accurate'],
-      'total_price' => att['total_price'] || att['total_paid_amount_accurate'],
-      'adults' => att['adults'] || att['guest_details']['number_of_adults'],
-      'children' => att['children'] || att['guest_details']['number_of_children'],
-      'infants' => att['infants'] || att['guest_details']['number_of_infants'],
-      'description' => att['guest_details']['localized_description'] || "#{att['infants'] + att['children'] + att['adults']} guests",
-    }
   end
 
-  def guest_attributes  
-    if params['guest'].present?
-      att = params.require(:guest).permit(:id, :first_name, :last_name, :phone, :email)
-      {
-        'id' => att['id'],
-        'first_name' => att['first_name'],
-        'last_name' => att['last_name'],
-        'phone_numbers' => [ att['phone'] ],
-        'email' => att['email'],
-      }
-    else
-      att = params.require(:reservation).permit(
-        :guest_email, :guest_id, :guest_last_name, :guest_first_name,
-        guest_phone_numbers: %i[]
-      )
-      {
-        'id' => att['guest_id'],
-        'first_name' => att['guest_first_name'],
-        'last_name' => att['guest_last_name'],
-        'phone_numbers' => att['guest_phone_numbers'],
-        'email' => att['guest_email'],
-      }
-    end
+  def guest_attributes(params)
+    guest_params(params).permit(:id, :first_name, :last_name, :phone_numbers, :email)
+  end
+
+  def reservation_params(params)
+    # editing params here before using strong params
+    # if more different payloads come in, this is where we adjust the mapping
+    # so that before we apply strong params, it's already a hash that the 
+    # api can recognize as attributes
+    params = params['reservation'].present? ? params['reservation'] : params
+    ActionController::Parameters.new({
+      'start_date' => params['start_date'],
+      'end_date' => params['end_date'],
+      'nights' => params['nights'],
+      'number_of_guests' => params['number_of_guests'] || params['guests'],
+      'status' => params['status'] || params['status_type'],
+      'currency' => params['currency'] || params['host_currency'],
+      'payout_price' => params['payout_price'] || params['expected_payout_amount'],
+      'security_price' => params['security_price'] || params['listing_security_price_accurate'],
+      'total_price' => params['total_price'] || params['total_paid_amount_accurate'],
+      'adults' => params['adults'] || params['guest_details']['number_of_adults'],
+      'children' => params['children'] || params['guest_details']['number_of_children'],
+      'infants' => params['infants'] || params['guest_details']['number_of_infants'],
+      'description' => description_string(params) || params['guest_details']['localized_description'],
+    })
+  end
+
+  def guest_params(params)
+    # again, editing the params here before applying strong params
+    ActionController::Parameters.new({
+      'id' => params.dig('guest', 'id') || params['guest_id'],
+      'first_name' => params.dig('guest', 'first_name') || params['guest_first_name'],
+      'last_name' => params.dig('guest', 'last_name') || params['guest_last_name'],
+      'phone_numbers' => [ params.dig('guest', 'phone') ] || params['guest_phone_numbers'],
+      'email' => params.dig('guest', 'email') || params['guest_email'],
+    })
+  end
+
+  def description_string(params)
+    return if !params.dig('infants')
+    "#{params['infants'] + params['children'] + params['adults']} guests"
   end
 end
